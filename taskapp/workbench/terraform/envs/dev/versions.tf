97537f4d374e4c03aa6ffa53b1f12fff3e04a0ce
Yohei Okabayashi <yohei.kuro48@gmail.com>
2024-05-02 22:38:12 +0900

    ADD: terraform

---
+++ taskapp/workbench/terraform/envs/dev/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "~>1.8.2"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~>5.27.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~>5.27.0"
+    }
+  }
+}
